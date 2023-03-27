00837d98a3cfed92b8d2c953d70fb04e07414d3a
Gerardo Juarez Salmoran <gerardo_juarez@hotmail.com>
2023-03-27 09:38:24 -0600

    se creo el proyecto tipo primitivo flotantes

--- Fundamentos/TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
+++ Fundamentos/TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -14,7 +14,29 @@ public class TiposPrimitivos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /*
+        Tipos primitivos enteros: byte, short, int, long
+         */
+        byte numeroByte = 12;
+        System.out.println("numeroByte = " + numeroByte);
+        System.out.println("valor minimo del byte" + Byte.MIN_VALUE);
+        System.out.println("valor maximo del byte" + Byte.MAX_VALUE);
+
+        short numeroShort = 10;
+        System.out.println("numeroShort = " + numeroShort);
+        System.out.println("valor minimo del short" + Short.MIN_VALUE);
+        System.out.println("valor maximo del short" + Short.MAX_VALUE);
+
+        int numeroInt =  999999;
+        System.out.println("numeroInt = " + numeroInt);
+        System.out.println("valor minimo del int" + Integer.MIN_VALUE);
+        System.out.println("valor maximo del int" + Integer.MAX_VALUE);
+
+        long numeroLong = 9;
+        System.out.println("numeroLong = " + numeroLong);
+        System.out.println("valor minimo del long" + Long.MIN_VALUE);
+        System.out.println("valor maximo del long" + Long.MAX_VALUE);
+
     }
-    
+
 }
