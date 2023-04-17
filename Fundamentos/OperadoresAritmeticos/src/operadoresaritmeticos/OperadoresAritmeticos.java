e9c845ecb00a5fbbb2252bce4dcbddf5c3493dd7
Gerardo Juarez <gerardo_juarez@hotmail.com>
2023-04-17 16:01:08 -0600

    se creo la clase operadores

---
+++ Fundamentos/OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=3, b=2;
+        var resultado = a + b;
+        System.out.println("resultado suma = " + resultado);
+        
+        resultado = a - b;
+        System.out.println("resultado resta = " + resultado);
+        
+        resultado = a * b;
+        System.out.println("resultado multiplicacion = " + resultado);
+        
+        var resultado2 = 3D / b;
+        System.out.println("resultado division = " + resultado2);
+        
+        resultado = a % b;
+        System.out.println("resultado modulo = " + resultado);
+        
+        if( b % 2 == 0)
+            System.out.println("Es numero par");
+        else
+            System.out.println("Es numero impar");
+    }
+    }
+    
+
---
+++ Fundamentos/OperadoresAsignacionComposicion/src/operadoresasignacioncomposicion/OperadoresAsignacionComposicion.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoresasignacioncomposicion;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class OperadoresAsignacionComposicion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 3, b=2;
+        int c = a + 5 - b; 
+        System.out.println("c = " + c);
+        
+        a += 1;//a = a + 1
+        System.out.println("a = " + a);
+        
+        a += 3;//a = a + 3
+        System.out.println("a = " + a);
+        
+        a -= 2;//a = a - 2
+        System.out.println("a = " + a);
+        
+        // *=   /=   %=
+    }
+    
+}
